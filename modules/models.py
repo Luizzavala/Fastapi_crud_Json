dfeaddc5a69d540d50a96887c2cdb900f50a3fb2
Luizzavala <luis.zavala@bancaprepa.com>
2022-03-05 12:18:00 -0700

    models

---
+++ modules/models.py
@@ -0,0 +1,44 @@
+#python
+from tkinter import N
+from typing import Optional
+from uuid import UUID
+from datetime import date, datetime
+
+#pydantic
+from pydantic import BaseModel, EmailStr, Field
+
+class UserBase(BaseModel):
+    user_id : UUID = Field(...)
+    email : EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(...,
+                          min_length=8,
+                          max_length=64)
+
+class User(UserBase):
+    first_name : str = Field(...,
+                             min_length=1,
+                             max_length=50,
+                             title="username",
+                             description="The username"
+                             )
+    last_name: str = Field(...,
+                            min_length=1,
+                            max_length=50,
+                            title="Lastname",
+                            description="The Lastname username"
+                            )
+    birth_date : Optional[date] =Field(default=None,
+                                       title="birthdate",
+                                       description="User´s birthdate ")
+    
+    
+class tweet(BaseModel):
+    tweet_id : UUID = Field(...)
+    content: str = Field(...,
+                         min_length=1,
+                         max_length=256)
+    created_at: datetime  = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    
